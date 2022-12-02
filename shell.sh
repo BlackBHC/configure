c4fb4234a9c809661f55dc938477ae99359ee7e0
bhchen <blackbhc@icloud.com>
2022-12-02 09:30:08 +0800

    Add in mkcd: mkdir + cd

--- shell.sh
+++ shell.sh
@@ -8,7 +8,9 @@
 ### alias
 #
 ### functions
-alias mkcd='function __mkcd(){ if [ $# == 1 ]; then mkdir $1; cd $1; unset -f __mkcd; elif [ $# == 2 ]; then mkdir $1 $2; cd $2; unset -f __mkcd; fi }; __mkcd'
+mkcd(){
+    mkdir $1 && cd $_
+}
 #
 
 
